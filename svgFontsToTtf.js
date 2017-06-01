7fde255786ffdf345dbaec156fa0f7f8f2b41ec4
rchaser53 <tayoshizawa29@gmail.com>
2017-06-02 00:07:01 +0900

    try to convert cli to script
    need to know the ttf2woff overview
    actually i don't know why this worked now

--- svgFontsToTtf.js
+++ svgFontsToTtf.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var svg2ttf = require('svg2ttf');
 
-var ttf = svg2ttf(fs.readFileSync('helloIcon.svg').toString(), {});
+var ttf = svg2ttf(fs.readFileSync('helloIcon.svg', 'utf8'), {});
 fs.writeFileSync('helloIcon.ttf', new Buffer(ttf.buffer));
