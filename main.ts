ebe7a3a693f1496f5634bdd2494c972bfd20a685
rchaser53 <tayoshizawa29@gmail.com>
2017-06-06 00:02:37 +0900

    hello electron

---
+++ main.ts
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false
+    }
+  });
+
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
---
+++ src/main.d.ts
@@ -0,0 +1,3 @@
+declare const app: any, BrowserWindow: any;
+declare let win: any;
+declare function createWindow(): void;
