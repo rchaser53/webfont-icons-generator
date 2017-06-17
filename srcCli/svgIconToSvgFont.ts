cc8af24dee73e1324187b2324ec35c096b2a38e5
rchaser53 <tayoshizawa29@gmail.com>
2017-06-17 22:39:06 +0900

    remove unnecessary files

--- srcCli/svgIconToSvgFont.ts
+++ srcCli/svgIconToSvgFont.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as svgicons2svgfont from 'svgicons2svgfont';
 
 export default (fileName: string, fontName: string, unicode: string): Promise<{}> => {
-  return new Promise((resolve, reject) => {
+  return new Promise<{}>((resolve, reject) => {
     const fontStream = svgicons2svgfont({ fontName });
 
     fontStream.pipe(fs.createWriteStream(`${fileName}Icon.svg`))
