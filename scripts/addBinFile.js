983bbf1dd9715ab8f45f8a479d62a623877ec0ef
rchaser53 <tayoshizawa29@gmail.com>
2017-06-28 22:16:34 +0900

    fix new line code

--- scripts/addBinFile.js
+++ scripts/addBinFile.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const fs = require('graceful-fs')
 
-let data = '#!/usr/bin/env node\r\n'
+// add new line
+let data = `#!/usr/bin/env node
+`
+
 const readStream = fs.createReadStream(path.resolve(__dirname, '../lib/index.js'), { encoding: 'utf8'})
 
 readStream.on('data', (_data) => {
@@ -9,7 +12,6 @@ readStream.on('data', (_data) => {
 })
 
 readStream.on('end', () => {
-  console.log(data)
   fs.writeFile(path.resolve(__dirname, '../lib/webfont-icons-generator.js'), data, { encoding: 'utf8'}, (err) => {
     if (err) throw new Error(err)
   })
